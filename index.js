db86feae687a5a39f002644b6caf2fcdb8b57420
lius <liuswin@163.com>
2020-05-26 10:37:05 +0800

    Release 1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   printMsg: function () {
     console.log('this message is from lius-test-pkg!');
-    console.log('change patch');
+    console.log('change patch v1.0.2');
   }
 }
