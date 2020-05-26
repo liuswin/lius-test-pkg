d500e22b538902374452c10ddf16080896a97686
lius <liuswin@163.com>
2020-05-26 10:28:30 +0800

    feat: change patch 1.0.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-    printMsg: function () {
-        console.log('this message is from lius-test-pkg!');
-    }
+  printMsg: function () {
+    console.log('this message is from lius-test-pkg!');
+    console.log('change patch');
+  }
 }
